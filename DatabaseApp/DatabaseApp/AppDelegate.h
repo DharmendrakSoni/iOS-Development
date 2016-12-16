905f87a73c2d3455be3b9228031f90588a9c896f
DharmendraKSoni <vinodbisht@vinods-Mac-mini.local>
2016-12-16 18:54:39 +0530

    GestureAppDemoCompleted

---
+++ DatabaseApp/DatabaseApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DatabaseApp
+//
+//  Created by vinod bisht on 17/08/16.
+//  Copyright © 2016 vinod bisht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
